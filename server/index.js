e6693774869fc65ec959a85d49f0c453a5043503
Florin Ardelian <florin.a@namastetechnologies.com>
2021-05-18 18:45:33 +0300

    Add support troop counter

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const fakes = require('./fakes')
+const fs = require('fs')
 const client = {
   fakes: require('../client/fakes'),
+  supportCounter: fs.readFileSync(`${__dirname}/../client/support-counter.js`),
 }
 
 const PORT = 8000
@@ -14,30 +16,36 @@ function err (res, e) {
   res.end('alert("error!")')
 }
 
-app.get('/fakes/random', function (req, res) {
-  try {
-    const server = (req.query.server || '').trim()
-    const tribes = (req.query.tribes || '').toLowerCase().split(' ').map(tribe => tribe.trim()).filter(Boolean)
-    const players = (req.query.players || '').toLowerCase().split(' ').map(player => player.trim()).filter(Boolean)
-    const coords = (req.query.coords || '').split(' ').map(coord => coord.trim()).filter(Boolean)
+app
+  .get('/fakes/random', function (req, res) {
+    try {
+      const server = (req.query.server || '').trim()
+      const tribes = (req.query.tribes || '').toLowerCase().split(' ').map(tribe => tribe.trim()).filter(Boolean)
+      const players = (req.query.players || '').toLowerCase().split(' ').map(player => player.trim()).filter(Boolean)
+      const coords = (req.query.coords || '').split(' ').map(coord => coord.trim()).filter(Boolean)
 
-    if (!server) {
-      res.status(400)
-      res.send(`Missing server`)
+      if (!server) {
+        res.status(400)
+        res.send(`Missing server`)
+      }
+
+      fakes.getRandomCoord(server, tribes, players, coords)
+        .then(data => {
+          if (!data) {
+            return res.end('alert("error!")')
+          }
+          client.fakes(req, res, data)
+        })
+        .catch(e => err(res, e))
+    } catch (e) {
+      err(res, e)
     }
+  })
 
-    fakes.getRandomCoord(server, tribes, players, coords)
-      .then(data => {
-        if (!data) {
-          return res.end('alert("error!")')
-        }
-        client.fakes(req, res, data)
-      })
-      .catch(e => err(res, e))
-  } catch (e) {
-    err(res, e)
-  }
-})
+  .get('/support-counter', function (req, res) {
+    res.header('Content-Type', 'text/javascript')
+    res.send(client.supportCounter)
+  })
 
 app.use('/static', express.static(`${__dirname}/../static`))
 
